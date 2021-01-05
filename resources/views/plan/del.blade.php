ee55359f7d1b580a156373328249591ffd828d53
Natsuki Inaba <n.inaba5555@gmail.com>
2021-01-05 15:12:46 +0900

    Plan.DeleteのUI修正

--- resources/views/plan/del.blade.php
+++ resources/views/plan/del.blade.php
@@ -2,34 +2,37 @@
 
 @section('title', 'Plan.Delete')
 
-@section('menubar')
-    @parent
-    削除ページ
+@section('header__btn--before')
+    <a href="/plan" class="header__btn">
+        <i class="fa fa-chevron-left"></i>
+    </a><!--header__btn END-->
+@endsection
+
+@section('header__btn--after')
+    <div class="header__btnSpacer"></div>
 @endsection
 
 @section('content')
-    <form action="/plan/del" method="post">
-        <table>
-            @csrf
-            <input type="hidden" name="id" value="{{$form->id}}">
-            <tr>
-                <th>Title</th>
-                <td>{{$form->title}}</td>
-            </tr>
-            <tr>
-                <th>Message</th>
-                <td>{{$form->message}}</td>
-            </tr>
-            <tr>
-                <th>Ideal</th>
-                <td>{{$form->s2m($form->ideal)}}</td>
-            </tr>
-            <tr>
-                <th></th>
-                <td><input type="submit" value="send"></td>
-            </tr>
-        </table>
-    </form>
+    <div class="section">
+        <h2 class="section__ttl">削除</h2>
+        <div class="panel">
+            <form class="form" action="/plan/del" method="post">
+                @csrf
+                <input type="hidden" name="id" value="{{$form->id}}">
+                <div class="panel__ttl">{{$form->title}}</div>
+                <div class="panel__message">{{$form->message}}</div>
+                <div class="panel__info panel__info--del">
+                    <div class="panel__info__ideal">予想 {{$form->s2m($form->ideal)}}</div> ｜
+                    @if ($form->reality == 0)
+                        <div class="panel__info__reality">未実行</div>
+                    @else
+                        <div class="panel__info__reality">実際 {{$form->s2m($form->reality)}}</div>
+                    @endif
+                </div><!--panel__info END-->
+                <button class="form__btn form__btn--del">削除</button>
+            </form>
+        </div><!--panel END-->
+    </div><!--section END-->
 @endsection
 
 @section('footer')
