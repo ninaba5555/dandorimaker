9f304d56c29b32462b6b01678d8d6a63a939955f
Natsuki Inaba <n.inaba5555@gmail.com>
2021-01-07 16:54:19 +0900

    Task.AddのUI修正

--- resources/views/task/add.blade.php
+++ resources/views/task/add.blade.php
@@ -2,53 +2,49 @@
 
 @section('title', 'Task.Add')
 
-@section('menubar')
-    @parent
-    新規作成ページ
+@section('header__btn--before')
+    @if (isset($plan_id))
+        <a href="/task?plan_id={{$plan_id}}" class="header__btn">
+            <i class="fa fa-chevron-left"></i>
+        </a><!--header__btn END-->
+    @else
+        <a href="/task" class="header__btn">
+            <i class="fa fa-chevron-left"></i>
+        </a><!--header__btn END-->
+    @endif
+@endsection
+
+@section('header__btn--after')
+    <div class="header__btnSpacer"></div>
 @endsection
 
 @section('content')
-    @if (count($errors) > 0)
-        <div>
-            <ul>
-                @foreach ($errors->all() as $error)
-                    <li>{{$error}}</li>
-                @endforeach
-            </ul>
-        </div>
-    @endif
-    <form action="/task/add" method="post">
-        <table>
-            @csrf
-            <tr>
-                <th>プランID</th>
+    <div class="section">
+        <h2 class="section__ttl">登録</h2>
+        <div class="panel">
+            @if (count($errors) > 0)
+                <div>
+                    <ul>
+                        @foreach ($errors->all() as $error)
+                            <li>{{$error}}</li>
+                        @endforeach
+                    </ul>
+                </div>
+            @endif
+            <form class="form" action="/task/add" method="post">
+                @csrf
                 @if (isset($plan_id))
-                    <td>
-                        {{$plan_id}}
-                        <input type="hidden" name="plan_id" value="{{$plan_id}}"></td>    
-                    </td>
+                    <input type="hidden" name="plan_id" value="{{$plan_id}}">
                 @else
-                    <td><input type="text" name="plan_id" value="{{old('plan_id')}}"></td>
+                    <label><input type="text" name="plan_id" value="{{old('plan_id')}}" placeholder="プランID"></label>
                 @endif
-            </tr>
-            <tr>
-                <th>Title</th>
-                <td><input type="text" name="title" value="{{old('title')}}"></td>
-            </tr>
-            <tr>
-                <th>Message</th>
-                <td><input type="text" name="message" value="{{old('message')}}"></td>
-            </tr>
-            <tr>
-                <th>Ideal</th>
-                <td><input type="number" name="ideal" value="{{old('ideal')}}">秒</td>
-            </tr>
-            <tr>
-                <th></th>
-                <td><input type="submit" value="send"></td>
-            </tr>
-        </table>
-    </form>
+                <label><input type="text" name="title" value="{{old('title')}}" placeholder="タイトル"></label>
+                <label><input type="text" name="message" value="{{old('message')}}" placeholder="メッセージ"></label>
+                <label><input type="number" name="ideal" value="{{old('ideal')}}" placeholder="予想時間"><div class="form__label__unit">秒</div></label>
+                <button class="form__btn">送信</button>
+            </form>
+        </div><!--panel END-->
+    </div><!--section END-->
 @endsection
 
 @section('footer')
