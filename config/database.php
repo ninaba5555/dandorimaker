91fec9613928a68292c98e7cec10220a759f71e7
ninaba5555 <n.inaba5555@gmail.com>
2021-01-08 15:41:54 +0900

    UNIXドメインソケットの設定

--- config/database.php
+++ config/database.php
@@ -61,6 +61,7 @@ return [
             'options' => extension_loaded('pdo_mysql') ? array_filter([
                 PDO::MYSQL_ATTR_SSL_CA => env('MYSQL_ATTR_SSL_CA'),
             ]) : [],
+            'unix_socket' => env('DB_UNIX_SOCKET', ''),
         ],
 
         'pgsql' => [
