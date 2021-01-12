d580f0c5a27dde6bca32e443487d18b2c1807144
Natsuki Inaba <n.inaba5555@gmail.com>
2021-01-12 16:32:46 +0900

    Task.EditのUI修正

--- resources/views/task/edit.blade.php
+++ resources/views/task/edit.blade.php
@@ -2,43 +2,45 @@
 
 @section('title', 'Task.Edit')
 
-@section('menubar')
-    @parent
-    編集ページ
+@section('header__btn--before')
+    @if (isset($plan_id))
+        <a href="/task?plan_id={{$plan_id}}" class="header__btn">
+            <i class="fa fa-chevron-left"></i>
+        </a><!--header__btn END-->
+    @else
+        <a href="/task" class="header__btn">
+            <i class="fa fa-chevron-left"></i>
+        </a><!--header__btn END-->
+    @endif
+@endsection
+
+@section('header__btn--after')
+    <div class="header__btnSpacer"></div>
 @endsection
 
 @section('content')
-    @if (count($errors) > 0)
-        <div>
-            <ul>
-                @foreach ($errors->all() as $error)
-                    <li>{{$error}}</li>
-                @endforeach
-            </ul>
-        </div>
-    @endif
-    <form action="/task/edit" method="post">
-        <table>
-            @csrf
-            <input type="hidden" name="id" value="{{$form->id}}">
-            <tr>
-                <th>Title</th>
-                <td><input type="text" name="title" value="{{$form->title}}"></td>
-            </tr>
-            <tr>
-                <th>Message</th>
-                <td><input type="text" name="message" value="{{$form->message}}"></td>
-            </tr>
-            <tr>
-                <th>Ideal</th>
-                <td><input type="number" name="ideal" value="{{$form->ideal}}">秒</td>
-            </tr>
-            <tr>
-                <th></th>
-                <td><input type="submit" value="send"></td>
-            </tr>
-        </table>
-    </form>
+    <div class="section">
+        <h2 class="section__ttl">編集</h2>
+        <div class="panel">
+            @if (count($errors) > 0)
+                <div>
+                    <ul>
+                        @foreach ($errors->all() as $error)
+                            <li>{{$error}}</li>
+                        @endforeach
+                    </ul>
+                </div>
+            @endif
+            <form class="form" action="/task/edit" method="post">
+                @csrf
+                <input type="hidden" name="id" value="{{$form->id}}">
+                <label><input type="text" name="title" value="{{$form->title}}" placeholder="タイトル"></label>
+                <label><input type="text" name="message" value="{{$form->message}}" placeholder="メッセージ"></label>
+                <label><input type="number" name="ideal" value="{{$form->ideal}}" placeholder="予想時間"><div class="form__label__unit">秒</div></label>
+                <button class="form__btn">送信</button>
+            </form>
+        </div><!--panel END-->
+    </div><!--section END-->
 @endsection
 
 @section('footer')
