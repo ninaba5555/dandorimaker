66a6f3c4e887edbf6cc7fea5fcd5480052b4fc03
Natsuki Inaba <n.inaba5555@gmail.com>
2021-01-15 12:55:39 +0900

    Plan.DoのUI修正

--- resources/views/plan/do.blade.php
+++ resources/views/plan/do.blade.php
@@ -2,52 +2,54 @@
 
 @section('title', 'Plan.Do')
 
-@section('menubar')
-    @parent
-    実行ページ
+@section('header__btn--before')
+    <a href="/plan" class="header__btn">
+        <i class="fa fa-chevron-left"></i>
+    </a><!--header__btn END-->
+@endsection
+
+@section('header__btn--after')
+    <div class="header__btnSpacer"></div>
     <script src="{{ asset('/js/dateformat.js') }}"></script>
     <script src="{{ asset('/js/timer.js') }}"></script>
 @endsection
 
 @section('content')
-    プラン情報
-    <table>
-        <tr>
-            <th>ID</th>
-            <th>Title</th>
-            <th>Message</th>
-            <th>Ideal</th>
-        </tr>
-        <tr>
-            <td>{{$plan->id}}</td>
-            <td>{{$plan->title}}</td>
-            <td>{{$plan->message}}</td>
-            <td>{{$plan->s2m($plan->ideal)}}</td>
-        </tr>
-    </table>
-    <hr />
-    タスク情報
-    <table>
-        <tr>
-            <th>ID</th>
-            <th>Title</th>
-            <th>Message</th>
-            <th>Ideal</th>
-        </tr>
-        <tr>
-            <td>
-                {{$task->id}}
-                <input type="hidden" name="task_id" value="{{$task->id}}">
-            </td>
-            <td>{{$task->title}}</td>
-            <td>{{$task->message}}</td>
-            <td>{{$task->s2m($task->ideal)}}</td>
-        </tr>
-    </table>
-    <div id="timer" class="timer"></div>
-    <button id="timer-btn" class="timer-btn" data-mode="start" onclick="timer({{$task->id}}, {{$task->ideal}})">
-        開始
-    </button>
+    <div class="section">
+        <h2 class="section__ttl">実行</h2>
+        <div class="panel">
+            <div class="panel__ttl">【プラン】{{$plan->title}}</div>
+            <div class="panel__message">{{$plan->message}}</div>
+            <div class="panel__info panel__info--del">
+                <div class="panel__info__ideal">予想 {{$plan->s2m($plan->ideal)}}</div> ｜
+                @if ($plan->reality == 0)
+                    <div class="panel__info__reality">未実行</div>
+                @else
+                    <div class="panel__info__reality">実際 {{$plan->s2m($plan->reality)}}</div>
+                @endif
+            </div><!--panel__info END-->
+        </div><!--panel END-->
+
+        <div class="panel">
+            <div class="panel__ttl">【実行タスク】{{$task->title}}</div>
+            <div class="panel__message">{{$task->message}}</div>
+            <div class="panel__info panel__info--del">
+                <div class="panel__info__ideal">予想 {{$task->s2m($task->ideal)}}</div> ｜
+                @if ($task->reality == 0)
+                    <div class="panel__info__reality">未実行</div>
+                @else
+                    <div class="panel__info__reality">実際 {{$task->s2m($task->reality)}}</div>
+                @endif
+            </div><!--panel__info END-->
+
+            <div id="timer" class="timer"></div>
+            <button id="timer-btn" class="timer-btn" data-mode="start" onclick="timer({{$task->id}}, {{$task->ideal}})">
+                開始
+            </button>
+        </div><!--panel END-->
+    </div><!--section END-->
+
+
 @endsection
 
 @section('footer')
